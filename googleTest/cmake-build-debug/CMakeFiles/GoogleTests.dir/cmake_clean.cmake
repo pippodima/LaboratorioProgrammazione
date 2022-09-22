f79abc3207e6ef0095103d69e2d0cab3cd1616ed
pippodima <pippodima99@gmail.com>
2022-09-22 17:53:27 +0200

    dopo mirko

---
+++ googleTest/cmake-build-debug/CMakeFiles/GoogleTests.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/GoogleTests.dir/runAllTests.cpp.obj"
+  "GoogleTests.exe"
+  "GoogleTests.exe.manifest"
+  "GoogleTests.pdb"
+  "libGoogleTests.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/GoogleTests.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
